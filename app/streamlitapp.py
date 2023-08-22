d62561b3473fcf3e1f8a1fd152a68243120ca556
juliaarisanda <juliaarisanda12@gmail.com>
2023-08-22 16:43:59 +0700

    upp

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -27,8 +27,8 @@ st.markdown(
 
 # Setup the sidebar
 with st.sidebar: 
-    image_path = os.path.join('logoss.jpg')
-    st.image(image_path, width=250)
+    # image_path = os.path.join('logoss.jpg')
+    # st.image(image_path, width=250)
     st.title('TalkClarity')
     st.info('Kenali gerakan bibir menjadi sebuah teks dengan membaca dari gerakan bibir.')
 
